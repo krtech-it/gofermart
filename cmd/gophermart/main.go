4125a0955134b69bafe16923b96ba34e70327a69
Кирилл Петухов <test@mail.ru>
2026-04-23 11:41:54 +0300

    feat: start main

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,3 +1,32 @@
 package main
 
-func main() {}
+import (
+	"github.com/krtech-it/gofermart/internal/config"
+	"github.com/krtech-it/gofermart/internal/delivery/http"
+	"github.com/krtech-it/gofermart/internal/handler"
+	"github.com/krtech-it/gofermart/internal/service"
+	"github.com/krtech-it/gofermart/internal/storage"
+	"log"
+)
+
+func main() {
+	cfg, err := config.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+	db, err := storage.NewPostgresStorage(cfg.DatabaseURI)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = db.Migrate("file://./migrations")
+	if err != nil {
+		log.Fatal(err)
+	}
+	services := service.NewServices(db, db, db, cfg)
+	handlers := handler.NewHandler(services)
+	router := http.NewRouter(handlers, cfg)
+	err = router.Run(cfg.RunAddress)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
